b3ffa94a02a2486074d86403cd76edf4bc6b7bfb
yohana simon <simonyohana597@gmail.com>
2025-10-17 19:43:28 +0200

    javascropt code

---
+++ index.js
@@ -0,0 +1,161 @@
+// function first() {
+//     console.log('first...');
+//      second()
+// }
+
+// function second() {
+//     console.log('second...');
+//      third()
+// }
+// function third() {
+//     console.log('third...');
+// }
+
+// first()
+
+
+// var x=100
+// var y=50
+// function getsum(n1, n2){
+//     var sum = n1 + n2
+//     return sum
+// }
+// var sum1 = getsum(x, y)
+// var sum2 = getsum(10, 5)
+
+// setTimeout(() =>{
+//     console.log ('hello from callback')
+// } ,2000)
+// console.log('run me!')
+
+// document.getElementById
+// ('text').addEventListener
+// ('click,') ,() => {
+
+// }
+// let name= 'john';
+// let age = 40;
+
+// let person = {
+//     name: 'bran',
+//     age: '30',
+// }
+
+// let newname = name;
+// newname = 'johnthan;'
+// console.log (name)
+
+// function person(name) {
+//     this.name = name;
+// }
+// const person1 = new person ('john');
+// const person2 = new person ('yohana');
+// const person3 = new person ('simon');
+
+// console.log("hello world");
+// //let declation
+// let name = "yohana";
+// //var declaton
+// var age = 30;
+// age=20;
+// const math = 3.14;
+// // pi=2.0;
+// console.log(math)
+// console.log("my name is yohana:",+age +" years",name)
+
+// console.log(name);
+// console.log(math)
+
+// let option =true;
+// console.log(option)
+// console.log(typeof option);
+// alert('hello world')
+
+
+// let mark = 90;
+// if (mark <90){
+//  console.log("you are allow to get a job")
+// }
+// else if (mark > 90){
+//   console.log("very good")
+// }
+// else{
+//     console.log("you need to improve")
+// }
+
+
+
+// let age = 19;
+// let text = (age >16) ? "you are child" : "abul";
+// console.log(text)
+
+
+// let day;
+// let date = new Date().getDay();
+ 
+// switch (date) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case  6:
+//     day = "Saturday";
+// }
+
+// console.log("our day is " + day)
+
+
+// let age = 25;
+// let year = 2025;
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+
+// console.log(points);
+
+// var score = 75;
+// console.log(score)
+
+// let name = "news"
+function learn(){
+    console.log("news")
+}
+learn();
+
+function calcutesum(sum1, sum2){
+    return sum1 + sum2
+}
+
+console.log(calcutesum(4, 8))
+
+let age = 18;
+let name = "yohana"
+if (age >= 18) {
+    console.log("vote")
+    if(name === "yohana"){
+        console.log("correct name")
+    }
+    else{
+        console.log("wrong name")
+    }
+    // else{
+    //     console.log("not allo")
+    // }
+}
